703bec4c24c31957e29abb08847e15abce1ecf5c
Alton <alton_ong@live.com.sg>
2021-08-27 22:48:48 +1000

    Changed themes of App and integrated Material UI typography

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,28 +1,14 @@
 import React from "react";
+import {Typography} from "@material-ui/core";
 import styled from '@emotion/styled'
 
-import { Paper, Button } from "@material-ui/core";
-
-const CustomButton = styled(Button)`
-	padding: 32px;
-	background-color: hotpink;
-	font-size: 24px;
-	border-radius: 4px;
-	color: black;
-	font-weight: bold;
-	&:hover {
-		color: white;
-	}
-`
-
 const Home = () => {
-	return <div>
-		<Paper elevation={3}>
-			<h1>Hello World!</h1>
-			<p>This is my first React App.</p>
-			<CustomButton>Click me</CustomButton>
-		</Paper>
-	</div>;
+	return(
+		<>
+			<Typography variant="h1" color="primary">Hello World!</Typography>
+			<Typography color="primary" variant="p">Welcome to the attendance builder</Typography>
+		</>
+	)
 };
 
 export default Home;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { CssBaseline } from "@material-ui/core";
+import {createTheme, ThemeProvider} from "@material-ui/core/styles";
+
+const theme = createTheme({
+    palette:{
+        primary: {
+          main: "#FFFFFF"
+        },
+        background: {
+            default: "#2B2D2F"
+        }
+    },
+    overrides: {
+        MuiCssBaseline: {
+            "@global": {
+                html: {
+                    margin: "0",
+                    padding: "0"
+                }
+            }
+        }
+    }
+})
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <ThemeProvider theme={theme}>
+        <CssBaseline/>
+        <App />
+    </ThemeProvider>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
